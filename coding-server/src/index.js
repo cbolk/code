f34c611d600b122e61841fe34ce354fa9230977b
Cristiana Bolchini <cristiana.bolchini@polimi.it>
2020-12-31 17:26:00 +0100

    adding access to monitoring db

--- coding-server/src/index.js
+++ coding-server/src/index.js
@@ -9,27 +9,37 @@ const lezioni = require('./lezioni');
 const soluzioni = require('./soluzioni');
 const recharge = require('./recharge');
 const attivita = require('./attivita');
+const meteo = require('./meteo');
 
-const connection = mysql.createConnection({
+const codingConn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'dbpass4root',
   database : 'coding'
 });
 
-connection.connect();
+const monitorConn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'usrmonitor',
+  password : 'pwdmonitor',
+  database : 'monitoring'
+});
+
+codingConn.connect();
+monitorConn.connect();
 
 const port = process.env.PORT || 8080;
 
 const app = express()
   .use(cors())
   .use(bodyParser.json())
-  .use(util(connection))
-  .use(esercizi(connection))
-  .use(esami(connection))
-  .use(lezioni(connection))
-  .use(soluzioni(connection))
-  .use(attivita(connection))
+  .use(util(codingConn))
+  .use(esercizi(codingConn))
+  .use(esami(codingConn))
+  .use(lezioni(codingConn))
+  .use(soluzioni(codingConn))
+  .use(attivita(codingConn))
+  .use(meteo(monitorConn))
   .use(recharge());
 
 app.listen(port, () => {
---
+++ coding-server/src/meteo.js
@@ -0,0 +1,62 @@
+const express = require('express');
+
+function createRouter(db) {
+  const router = express.Router();
+
+  router.get('/meteo/day/:day', function (req, res, next) {
+    var day = req.params.day;
+    day = day.substr(0, 4) + "-" + day.substr(4, 2) + "-" + day.substr(6, 2);
+    var strSQL ="SELECT * FROM meteo WHERE ts >= '" + day + "' and ts < DATE_ADD('" + day + "', INTERVAL 1 DAY) ORDER BY ts;";
+    console.log(strSQL);
+    db.query(strSQL,
+      (error, results) => {
+          if (error) {
+            console.log(error);
+            console.log(strSQL);
+            res.status(500).json({status: 'error'});
+          } else {
+            res.status(200).json(results);
+          }
+        }
+      );
+  });
+
+  router.get('/meteo/month/:year/:month', function (req, res, next) {
+    var year = req.params.year;
+    var month = req.params.month;
+    var strSQL ="SELECT * FROM meteo WHERE EXTRACT(YEAR FROM ts) = " + year + " and EXTRACT(MONTH FROM ts) = " + month + " ORDER BY ts;";
+    console.log(strSQL);
+    db.query(strSQL,
+      (error, results) => {
+          if (error) {
+            console.log(error);
+            res.status(500).json({status: 'error'});
+          } else {
+            res.status(200).json(results);
+          }
+        }
+      );
+  });
+
+  router.get('/meteo/last30days/:day', function (req, res, next) {
+    var day = req.params.day;
+    day = day.substr(0, 4) + "-" + day.substr(4, 2) + "-" + day.substr(6, 2);
+    var strSQL ="SELECT * FROM meteo WHERE ts > DATE_ADD('" + day + "' , INTERVAL -30 DAY) and ts <='" + day + "' ORDER BY ts;";
+    console.log(strSQL);
+    db.query(strSQL,
+      (error, results) => {
+          if (error) {
+            console.log(error);
+            console.log(strSQL);
+            res.status(500).json({status: 'error'});
+          } else {
+            res.status(200).json(results);
+          }
+        }
+      );
+  });
+
+  return router;
+}
+
+module.exports = createRouter;
